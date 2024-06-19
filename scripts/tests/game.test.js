181518fc7e4a939ea133b89083cc4003c1f700a5
MrMarlonM <marlon.martynkiewitz@googlemail.com>
2024-06-19 09:38:35 +0200

    Add addTurn and lightsOn function, add tests for both functions

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-const { game, newGame, showScore } = require("../game");
+const { game, newGame, showScore, addTurn, lightsOn } = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -43,10 +43,33 @@ describe("newGame works correctly", () => {
     test("Should clear playerMoves array", () => {
         expect(game.playerMoves).toEqual([]);
     });
-    test("Should clear currentGame array", () => {
-        expect(game.currentGame).toEqual([]);
+    test("Should be one move in computer's game array", () => {
+        expect(game.currentGame.length).toBe(1);
     });
     test("Should display 0 for the element with id of score", () => {
         expect(document.getElementById("score").innerText).toEqual(0);
     })
+});
+
+describe("gameplay works correctly", () => {
+    beforeEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+        addTurn();
+    });
+    afterEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+    });
+    test("AddTurn adds a new turn to the game", () => {
+        addTurn();
+        expect(game.currentGame.length).toBe(2);
+    });
+    test("Should add correct class to light up the buttons", () => {
+        let button = document.getElementById(game.currentGame[0]);
+        lightsOn(game.currentGame[0]);
+        expect(button.classList).toContain("light");
+    });
 });
