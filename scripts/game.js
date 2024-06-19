4fd83dc92a330bff6bf92f120bff14c29264ad3d
MrMarlonM <marlon.martynkiewitz@googlemail.com>
2024-06-19 10:32:44 +0200

    Add showTurns function, add tests for showTurns function

--- scripts/game.js
+++ scripts/game.js
@@ -2,6 +2,7 @@ let game = {
     score: 0,
     currentGame: [],
     playerMoves: [],
+    turnNumber: 0,
     choices: ["button1", "button2", "button3", "button4"],
 }
 
@@ -11,6 +12,7 @@ function newGame() {
     game.currentGame = [];
     showScore();
     addTurn();
+    showTurns();
 }
 
 function showScore() {
@@ -30,4 +32,15 @@ function lightsOn(circ) {
     }, 400);
 }
 
-module.exports = { game, newGame, showScore, addTurn, lightsOn };
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(() => {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+        }
+    }, 800);
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
